73a0a0ce387da28ad5e5cfb5420fe37c9b7ec877
jcymeow <20970736@qq.com>
2026-02-18 01:36:00 +0800

    v1.4.1A
    修正 简中翻译中充斥繁中问题.

--- jcy/file_operations.py
+++ jcy/file_operations.py
@@ -1785,11 +1785,7 @@ class FileOperations:
             data_path = os.path.join(MOD_PATH, r"jcy/config/item-runes.data.json")
             with open(data_path, 'r', encoding='utf-8') as f:
                 data_dict = json.load(f)
-            # 松岗简/繁体, 采用简/繁体数据
-            for key, obj in data_dict.items():
-                obj[Language.SGCN.value] = obj[Language.ZHCN.value]
-                obj[Language.SGTW.value] = obj[Language.ZHTW.value]
-
+            
             # 结果集
             ext_json = {}
 
